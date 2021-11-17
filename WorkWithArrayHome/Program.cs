0be027e7d795472fe929e2c8c30a5fc75ae8dceb
DmitriyFedan <dmitry061093@mail.ru>
2021-11-17 14:09:18 +0700

    поправил ввод

--- WorkWithArrayHome/Program.cs
+++ WorkWithArrayHome/Program.cs
@@ -2,11 +2,28 @@
 
 while (true)
 {
+    Console.Clear();
     Console.WriteLine("==      Ищем второй наибольший элемент массива      == ");
     Console.Write("Введите число элементов  предполагаемого массива: ");
+    int count;
+   
     try
     {
-        int count = Convert.ToInt32(Console.ReadLine()); // тут может быть ex 
+        if (!int.TryParse(Console.ReadLine(), out count)) // на случай если вводят не число
+        {
+            Console.WriteLine("Не удалось преобразовать введенное значение в целое число");
+            Console.WriteLine("Нажмите любую клавишу чтобы начать с начала");
+            Console.ReadKey();
+            continue;
+        }
+        if (count < 2)  // на случай  пытаются искать в массиве в котором мене 2 элентов
+        {
+            Console.WriteLine("В массиве менее 2 элементов");
+            Console.WriteLine("Нажмите любую клавишу чтобы начать с начала");
+            Console.ReadKey();
+            continue;
+        }
+        //int count = Convert.ToInt32(Console.ReadLine()); // тут может быть ex 
         int[] user_array = new int[count] ;
         for (int i=0; i< user_array.Length; i++)
         {
@@ -18,20 +35,21 @@ while (true)
             Console.WriteLine("\n");
             Console.Write("Вводите элементы массива целыми числами, по одному: ");
             user_array[i] = Convert.ToInt32(Console.ReadLine()); //   тут может быть ex 
-        }
-        
-        int result = SearchInArr(user_array);
+        }    
+        int result = SearchInArr(user_array); 
         Console.WriteLine($"Второй по наибольший элемент массива = {result}");
+        Console.WriteLine("Нажмите любую клавишу чтобы начать с начала");
+        Console.ReadKey();
     }
-    catch
+    catch(Exception ex) /// на случай тех ошибок что я не смог предусмотреть
     {
-        Console.WriteLine("Возникла ошибка");
+        Console.WriteLine("Возникла  неизвестная ошибка");
+        Console.WriteLine(ex.Message);
         Console.WriteLine("Нажмите любую клавишу чтобы начать с начала");
         Console.ReadKey();
         Console.Clear();
         continue;
     }
-
 }
 
 static int SearchInArr(int[] array)
@@ -50,5 +68,7 @@ static int SearchInArr(int[] array)
             secondMaxVal = item;
         }
     }
+    if (maxVal == secondMaxVal) // если все элементы одинвковые 
+        throw new Exception(" В массиве невозможно найти второй максималньый элемент"); 
     return secondMaxVal;
 }
