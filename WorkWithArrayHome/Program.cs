55fddd4878f0a144287c7eaf538a831cb666f0e8
DmitriyFedan <dmitry061093@mail.ru>
2021-11-17 12:30:02 +0700

    сделал  обработку пользовательского ввода

--- WorkWithArrayHome/Program.cs
+++ WorkWithArrayHome/Program.cs
@@ -1,19 +1,54 @@
 ﻿
 
-int[] array = new int[8] { 1, 3, 5, 9, 4, 8, 1, 9 };
-
-int maxVal = array[0];
-int secondMaxVal = array[0];
-foreach (int item in array)
+while (true)
 {
-    if (item > maxVal) // ищем наибольший 
+    Console.WriteLine("==      Ищем второй наибольший элемент массива      == ");
+    Console.Write("Введите число элементов  предполагаемого массива: ");
+    try
     {
-        secondMaxVal = maxVal;
-        maxVal = item;
+        int count = Convert.ToInt32(Console.ReadLine()); // тут может быть ex 
+        int[] user_array = new int[count] ;
+        for (int i=0; i< user_array.Length; i++)
+        {
+            Console.Clear();
+            Console.WriteLine("==      Ищем второй наибольший элемент массива      == ");
+            Console.Write("Ваш массив: ");
+            foreach (int item in user_array)
+                Console.Write($"{item} ");
+            Console.WriteLine("\n");
+            Console.Write("Вводите элементы массива целыми числами, по одному: ");
+            user_array[i] = Convert.ToInt32(Console.ReadLine()); //   тут может быть ex 
+        }
+        
+        int result = SearchInArr(user_array);
+        Console.WriteLine($"Второй по наибольший элемент массива = {result}");
     }
-    else if (item > secondMaxVal && item < maxVal) // проверяем лежит ли текущий в диапазоне от наибольшего до второго наибольшего
+    catch
     {
-        secondMaxVal = item;
+        Console.WriteLine("Возникла ошибка");
+        Console.WriteLine("Нажмите любую клавишу чтобы начать с начала");
+        Console.ReadKey();
+        Console.Clear();
+        continue;
     }
+
 }
-Console.WriteLine($"Второй по наибольший элемент массива = {secondMaxVal}") ;
+
+static int SearchInArr(int[] array)
+{
+    int maxVal = array[0];
+    int secondMaxVal = array[0];
+    foreach (int item in array)
+    {
+        if (item > maxVal) //  если есть новый макс. старый  записываем во второй макс 
+        {
+            secondMaxVal = maxVal;
+            maxVal = item;
+        }
+        else if (item > secondMaxVal && item < maxVal) // проверяем лежит ли текущий в диапазоне от наибольшего до второго наибольшего
+        {
+            secondMaxVal = item;
+        }
+    }
+    return secondMaxVal;
+}
