434787324c3a34b1740226819ac1a3b983f66738
DmitriyFedan <dmitry061093@mail.ru>
2021-11-19 12:58:08 +0700

    Закончил написание класса,

--- WorkWithClassVector/Vector.cs
+++ WorkWithClassVector/Vector.cs
@@ -19,14 +19,22 @@ namespace WorkWithClassVector
             this.z = z;
         }
 
-        public double VectorLength()
+        /// <summary>
+        /// Method find the length of passed vector
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <returns></returns>
+        public static double VectorLength(Vector v1)
         {
-            double lenth = Math.Sqrt(x * x + y * y + z * z);
+            double lenth = Math.Sqrt(v1.x * v1.x +
+                                     v1.y * v1.y +
+                                     v1.z * v1.z);
             return lenth;
         }
 
         public static int ScalarProduct(Vector v1, Vector v2)
         {
+
             int scalar = (v1.x * v2.x +
                           v1.y * v2.y +
                           v1.z * v2.z);
@@ -35,13 +43,30 @@ namespace WorkWithClassVector
         public static Vector VectorProduct(Vector v1, Vector v2)
         {
             int res_x = v1.y * v2.z - v1.z * v2.y;
-            int res_y = v1.z*v2.x - v1.x * v2.z;
+            int res_y = v1.z * v2.x - v1.x * v2.z;
             int res_z = v1.x * v2.y - v1.y * v2.x;
             Vector resvect = new Vector(res_x, res_y, res_z);
             return resvect;
         }
 
+        public static double AngleAmongVectors(Vector v1, Vector v2)
+        {
+            double angle = (ScalarProduct(v1, v2) /
+                            (Math.Abs(Vector.VectorLength(v1)) *
+                            (Math.Abs(Vector.VectorLength(v2)))));
+            return angle;
+        }
 
+        public static Vector SumVectors(Vector v1, Vector v2)
+        {
+            Vector sumvector = new Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+            return sumvector;
+        }
+        public static Vector DiffVectors(Vector v1, Vector v2)
+        {
+            Vector diffvector = new Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+            return diffvector;
+        }
     }
 
 }
