77e033be9122a6e80de0ad6091be8013de8148d3
DmitriyFedan <dmitry061093@mail.ru>
2021-12-02 16:15:32 +0700

    Добавил новый проект , создал классы и реализовал подписку при помощи событий

---
+++ CounterWithEvents/Counter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterWithEvents
+{
+    public class Counter
+    {
+
+        public delegate void MyDelegate(); 
+        public event MyDelegate MyEvent;
+        public int num { get; init; } //  будем записывать сюда искомое число при сосздании
+        public Counter(int numberForEvent)
+        { 
+            num = numberForEvent;
+        }
+        public void Count()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i == num)
+                {
+                    MyEvent?.Invoke();
+                }
+            }
+        }    
+    }
+
+    class Handler1
+    {
+        public void GetMessage()
+        {
+            Console.WriteLine("пора действовать ведь уже 77");
+        }
+    }
+
+    class Handler2
+    {
+        public void GetMessage()
+        {
+            Console.WriteLine("уже 77 давно пора было начать");
+        }
+    }
+
+}
---
+++ CounterWithEvents/Program.cs
@@ -0,0 +1,22 @@
+﻿using CounterWithEvents;
+
+try
+{
+    Counter myCounter = new Counter(77);
+
+    Handler1 handler1 = new Handler1();
+    Handler2 handler2 = new Handler2();
+    // добавление двух подписок
+    myCounter.MyEvent += handler1.GetMessage; 
+    //myCounter.MyEvent += handler2.GetMessage;
+
+    myCounter.Count();
+    // одна отписка от события
+   // myCounter.MyEvent -= handler2.GetMessage;
+    myCounter.Count();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Возникла неизвестная ошибка {ex.Message}");
+}
+
