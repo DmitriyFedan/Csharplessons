c23db3e5c22b06955ead46f888cb262eb119362d
DmitriyFedan <dmitry061093@mail.ru>
2021-12-03 13:33:36 +0700

    реализовал базовый функционал stringsearcher и добавил аппробацию в program

---
+++ StringSearcher/Program.cs
@@ -0,0 +1,25 @@
+﻿using SearchInString;
+
+var searcher = new StringSearcher();
+searcher.MyEvent += Handler;  // подписка
+
+List<string> exampleList = new List<string>() {"QWER", "qwer", "REWQ", "1234", "rewq", "123qwer", "REWQ4321" };
+
+
+string exampleString = "qwe";
+//string exampleString = "123";
+//string exampleString = "vbn";
+//string exampleString = "EWQ";
+
+searcher.Search(exampleList, exampleString);
+
+searcher.MyEvent -= Handler;  // отписка
+//пробуем еще раз
+searcher.Search(exampleList, exampleString);
+
+static void Handler(string inputstring)
+{
+    Console.WriteLine($"Результаты поиска: {inputstring}");
+}
+
+
---
+++ StringSearcher/StringSearcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SearchInString
+{
+    public class StringSearcher
+    {
+        public delegate void myDelegate(string s);
+        public event myDelegate MyEvent;
+        
+        private bool flag = false;
+
+        public void Search(List<string> mylst, string mystr)
+        {
+            //  проверяем как работают разные вар-ты с regex
+            
+            foreach (string item in mylst)
+            {
+                Regex regex = new Regex($"{mystr}", RegexOptions.IgnoreCase);
+                //Regex regex = new Regex(@$"{mystr}(/w*)", RegexOptions.RightToLeft);
+                //Regex regex = new Regex($"{mystr}(/w*)");
+                MatchCollection match = regex.Matches(item);
+                if (match.Count > 0)
+                {
+                    flag = true;
+                    foreach (Match matchitem in match)
+                    {
+                        //Console.WriteLine(matchitem.Value);
+                        MyEvent?.Invoke(item);    
+                    }
+                }               
+            }
+            if (!flag) MyEvent("Совпадений не найдено");
+        }
+    }
+}
