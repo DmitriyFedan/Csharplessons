b4e5aef00ba59ff79dd679d75ea9fc00a52527c1
DmitriyFedan <dmitry061093@mail.ru>
2021-12-15 14:59:06 +0700

    реализовал считывание файла, и вывод сообщений о начале и конце считывания

--- AsyncAwait/Program.cs
+++ AsyncAwait/Program.cs
@@ -1,2 +1,39 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using System.Threading.Tasks;
+using System.Threading;
+//using System.IO;
+
+
+static async void FileReaderAsync()
+{
+    string filePath = @"G:\WORKED\C# projects\Csharplessons\AsyncAwait\Test.txt";
+    if (File.Exists(filePath))
+    {
+        Console.WriteLine("Начало считывания файла");
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string result = await reader.ReadToEndAsync();
+            Thread.Sleep(10000);
+            Console.WriteLine("Считывание завершено");
+            Console.WriteLine($"считали текст: {result}");
+        }
+    }
+    else Console.WriteLine($"Не удалось найти файл по указанному пути {filePath}");
+}
+
+static void FileReaderBreaker()
+{
+    CancellationTokenSource cts = new CancellationTokenSource();
+    CancellationToken token = cts.Token;
+
+    Console.WriteLine("Для прерывания считывания файла нажмите X");
+    ConsoleKeyInfo key = Console.ReadKey();
+    Console.WriteLine(key.KeyChar);
+
+
+}
+FileReaderAsync();
+FileReaderBreaker();
+
+Console.Read();
+
